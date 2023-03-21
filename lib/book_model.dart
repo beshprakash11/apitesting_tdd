8a9602441ee20e8d92c595cd13fdd44c2b4f04d2
beshprakash11 <beshprakashktm@gmail.com>
2023-03-21 11:35:54 +0100

    from json created

--- lib/book_model.dart
+++ lib/book_model.dart
@@ -10,4 +10,11 @@ class BooksListModel {
   String auther;
   String decription;
   String amazon;
+
+  factory BooksListModel.fromJson(Map<String, dynamic> json) => BooksListModel(
+        name: json["name"],
+        auther: json["auther"],
+        decription: json["decription"],
+        amazon: json["amazon"],
+      );
 }
