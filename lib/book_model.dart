e320fc4c3d995ef3809a8130c41cf5ccf70ced67
beshprakash11 <beshprakashktm@gmail.com>
2023-03-21 11:35:26 +0100

    bool list model created with required property and constructor

--- lib/book_model.dart
+++ lib/book_model.dart
@@ -1,3 +1,13 @@
 class BooksListModel {
-  
+  BooksListModel({
+    required this.name,
+    required this.auther,
+    required this.decription,
+    required this.amazon,
+  });
+
+  String name;
+  String auther;
+  String decription;
+  String amazon;
 }
