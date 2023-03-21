d79fa26db6109be0bdea009c64c7047246e90554
beshprakash11 <beshprakashktm@gmail.com>
2023-03-21 11:49:42 +0100

    return fetch data

--- lib/fetch_book.dart
+++ lib/fetch_book.dart
@@ -1,2 +1,19 @@
+import 'dart:convert';
+
+import 'package:apitesting_tdd/book_model.dart';
+import 'package:http/http.dart' as http;
+
 String fetchBooksURL =
     'https://raw.githubusercontent.com/Richa0305/mock-json/main/book.json';
+Future<List<BooksListModel>> fetchBooks(http.Client client) async {
+  final response = await client.get(Uri.parse(fetchBooksURL));
+  if (response.statusCode == 200) {
+    return List<BooksListModel>.from(
+      json.decode(response.body).map(
+            (x) => BooksListModel.fromJson(x),
+          ),
+    );
+  } else {
+    throw Exception('Failed to load books data');
+  }
+}
