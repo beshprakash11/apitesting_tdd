241c3fca1a2b0a54caac4f23065aed4cd293e6a3
beshprakash11 <beshprakashktm@gmail.com>
2023-03-22 07:55:35 +0100

    book model updated

--- lib/book_model.dart
+++ lib/book_model.dart
@@ -2,14 +2,14 @@ class BooksListModel {
   BooksListModel({
     required this.name,
     required this.auther,
-    required this.decription,
-    required this.amazon,
+    this.decription,
+    this.amazon,
   });
 
   String name;
   String auther;
-  String decription;
-  String amazon;
+  String? decription;
+  String? amazon;
 
   factory BooksListModel.fromJson(Map<String, dynamic> json) => BooksListModel(
         name: json["name"],
