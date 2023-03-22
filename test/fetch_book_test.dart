903d5020e1c62be3aba7e370fd97795a281e04ac
beshprakash11 <beshprakashktm@gmail.com>
2023-03-22 07:39:26 +0100

    mock generator created

--- test/fetch_book_test.dart
+++ test/fetch_book_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
 
+@GenerateMocks([http.Client])
 void main() {
   group('Fetch Books api call test', () {
     
