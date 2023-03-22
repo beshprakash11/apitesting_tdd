3ce07068c39f1e91edd419631497a807013400be
beshprakash11 <beshprakashktm@gmail.com>
2023-03-22 07:47:50 +0100

    mock cliet arrange define

--- test/fetch_book_test.dart
+++ test/fetch_book_test.dart
@@ -1,15 +1,26 @@
+import 'package:apitesting_tdd/fetch_book.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'fetch_book_test.mocks.dart';
 
 @GenerateMocks([http.Client])
 void main() {
-
   //Fetch Books api call test
   group('Fetch Books api call test', () {
     //Should return list of books for http success call
-    test('Should return list of books for http success call', (){
-      
+    test('Should return list of books for http success call', () {
+      final mockClint = MockClient();
+
+      when(
+        mockClint.get(
+          Uri.parse(fetchBooksURL),
+        ),
+      ).thenAnswer(
+        (realInvocation) async => http.Response('', 200),
+      );
     });
   });
 }
